716183501f0ea677e27a9d0afb46cff048956379
Stig Otnes Kolstad <stigok@broentech.no>
2018-01-15 02:27:34 +0100

    Alarm as module

---
+++ alarm.js
@@ -0,0 +1,36 @@
+const Emitter = require('events')
+
+class Alarm extends Emitter {
+  constructor ({timeout}) {
+    super()
+
+    console.log('timeout', timeout)
+    if (!timeout || typeof timeout !== 'number') {
+      throw new Error('Must specify a timeout')
+    }
+
+    this._timeout = timeout
+  }
+
+  stop () {
+    clearTimeout(this._alarmTimer)
+  }
+
+  start () {
+    this.stop()
+    this.extend()
+  }
+
+  extend () {
+    this.emit('extend', this._timeout)
+
+    clearTimeout(this._alarmTimer)
+
+    this._alarmTimer = setTimeout(() => {
+      this.emit('alarm')
+    }, this._timeout)
+  }
+}
+
+module.exports = Alarm
+
--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const assert = require('assert')
-const HostMon = require('./host-monitor')
+const Alarm = require('./alarm')
 const Logger = require('./logger')
+const Pinger = require('./pinger')
 
 const timeout = 10000
 const host = process.argv[2]
 assert(host, 'Missing first argument <host>')
 
 const logger = new Logger(host)
-const mon = new HostMon({ host, timeout })
-
-mon.on('ping', (ms) => logger.info('Host answered', ms))
-mon.on('dead', () => logger.error('Host has not responded within timeout...'))
+const pinger = new Pinger({ host })
+const alarm = new Alarm({ timeout: 15000 })
 
 logger.info('Monitoring host %s', host)
 logger.info('Will report dead if not responding within %dms', timeout)
+
+pinger.on('data', (res) => {
+  if (res.status === 'up') {
+    alarm.extend()
+  } else {
+    logger.info('Missed last reply')
+  }
+})
+
+alarm.on('alarm', () => logger.error('Host did not respond within time limit'))
 
